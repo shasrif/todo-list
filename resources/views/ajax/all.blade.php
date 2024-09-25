1e19a18c12f34a7c19053795b9fb0c13b422cfd9
T M Sharifur Rahman <shasrif@gmail.com>
2024-09-25 17:08:32 +0600

    Update all.blade.php

--- resources/views/ajax/all.blade.php
+++ resources/views/ajax/all.blade.php
@@ -3,7 +3,7 @@
     <th scope="row">{{ $row->id }}</th>
     <td>{{ $row->title }}</td>
     <td>{{ $row->body }}</td>
-    <td></td>
+    <td>{{ $row->user->name }}</td>
     <td class="status-{{$row->id}}">{!! $row->status == 0 ? '<a class="btn btn-info" href="javascript:;" onClick="change_status('.$row->id.', '.$row->status.')">Pending</a>' : '<a class="btn btn-info" href="javascript:;" onclick="change_status('.$row->id.', '.$row->status.')">Completed</a>' !!}</td>
     <td>
         <a class="btn btn-primary" href="javascript:;" onClick="todo_edit({{ $row->id }})">Edit</a>
